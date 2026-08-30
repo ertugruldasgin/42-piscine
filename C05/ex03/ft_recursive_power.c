9285a58c1dc845fb025a43e88da4e8ea4b412617
Umut Ertuğrul Daşgın <ertugruldasgin@gmail.com>
2026-08-30 16:44:16 +0300

    add C05:ex03

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -1,3 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/30 16:39:50 by udasgin           #+#    #+#             */
+/*   Updated: 2026/08/30 16:43:32 by udasgin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_power(int nb, int power)
 {
+	if (power < 0)
+		return (0);
+	if (power == 0 || nb == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
 }
