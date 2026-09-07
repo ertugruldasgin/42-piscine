f7b88ef473f99522a365b41594ba8f0dc2f00c14
ertugruldasgin <105071264+ertugruldasgin@users.noreply.github.com>
2026-09-07 14:47:21 +0300

    add C11: ex06

--- C11/ex06/ft_sort_string_tab.c
+++ C11/ex06/ft_sort_string_tab.c
@@ -1,1 +1,54 @@
-void	ft_sort_string_tab(char **tab);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/07 14:29:48 by udasgin           #+#    #+#             */
+/*   Updated: 2026/09/07 14:46:52 by udasgin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	while (*str1 && *str2 && *str1 == *str2)
+	{
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int	i;
+	int	flag;
+
+	flag = 1;
+	while (flag)
+	{
+		flag = 0;
+		i = 0;
+		while (tab[i] && tab[i + 1])
+		{
+			if (strcmp(tab[i], tab[i + 1]) > 0)
+			{
+				ft_swap(&tab[i], &tab[i + 1]);
+				flag = 1;
+			}
+			i++;
+		}
+	}
+}
