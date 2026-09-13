f567c4b742ea0917962b63a30733daaea9ed9437
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-09-13 14:46:16 +0300

    last version of C04

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,10 +6,67 @@
 /*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 01:48:10 by udasgin           #+#    #+#             */
-/*   Updated: 2026/08/30 02:12:50 by udasgin          ###   ########.fr       */
+/*   Updated: 2026/09/13 14:45:04 by udasgin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	to_base_format(long num, char *base)
+{
+	long	len;
+
+	len = ft_strlen(base);
+	if (num >= len)
+		to_base_format(num / len, base);
+	write(1, &base[num % len], 1);
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
+	long	num;
+
+	num = nbr;
+	if (!is_valid_base(base))
+		return ;
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		num *= -1;
+	}
+	to_base_format(num, base);
 }
--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -6,10 +6,85 @@
 /*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/02 02:05:14 by udasgin           #+#    #+#             */
-/*   Updated: 2026/09/02 02:05:18 by udasgin          ###   ########.fr       */
+/*   Updated: 2026/09/13 14:44:09 by udasgin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' ' || (base[i] >= 9
+				&& base[i] <= 13))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	get_index(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
 int	ft_atoi_base(char *str, char *base)
 {
+	int	res;
+	int	sign;
+	int	digit;
+
+	if (!is_valid_base(base))
+		return (0);
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	digit = get_index(*str, base);
+	res = 0;
+	while (digit != -1)
+	{
+		res = res * ft_strlen(base) + digit;
+		str++;
+		digit = get_index(*str, base);
+	}
+	return (sign * res);
 }
