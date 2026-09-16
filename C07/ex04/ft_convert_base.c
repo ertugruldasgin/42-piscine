0b5be5cc0a58baee2f796891a134ccaf7ba1ceea
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-09-16 13:31:36 +0300

    add C07:ex04

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,10 +6,79 @@
 /*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/02 16:36:23 by udasgin           #+#    #+#             */
-/*   Updated: 2026/09/02 17:16:17 by udasgin          ###   ########.fr       */
+/*   Updated: 2026/09/16 13:20:28 by udasgin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int		ft_strlen(char *str);
+int		is_valid_base(char *base);
+int		ft_atoi_base(char *str, char *base);
+
+int	num_len(long num, int base_len)
+{
+	int	len;
+
+	len = 0;
+	if (num < 0)
+	{
+		num = -num;
+		len++;
+	}
+	if (num == 0)
+		return (1);
+	while (num)
+	{
+		num /= base_len;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa_base(int nbr, char *base)
+{
+	long	num;
+	int		len;
+	int		base_len;
+	char	*res;
+
+	base_len = ft_strlen(base);
+	num = nbr;
+	len = num_len(num, base_len);
+	res = malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	res[len] = '\0';
+	if (num < 0)
+	{
+		res[0] = '-';
+		num = -num;
+	}
+	if (num == 0)
+		res[0] = base[0];
+	while (num)
+	{
+		res[--len] = base[num % base_len];
+		num /= base_len;
+	}
+	return (res);
+}
+
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
+	if (!is_valid_base(base_from) || !is_valid_base(base_to))
+		return (NULL);
+	return (ft_itoa_base(ft_atoi_base(nbr, base_from), base_to));
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_convert_base("ff", "0123456789abcdef", "01"));
+	printf("%s\n", ft_convert_base("42", "0123456789", "01"));
+	printf("%s\n", ft_convert_base("0", "0123456789", "01"));
+	printf("%s\n", ft_convert_base("12z34", "0123456789", "0123456789"));
+	printf("%s\n", ft_convert_base("1", "01", "0"));
+	return (0);
 }
---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 17:52:56 by udasgin           #+#    #+#             */
+/*   Updated: 2026/09/15 17:58:31 by udasgin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' ' || (base[i] >= 9
+				&& base[i] <= 13))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	get_index(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	res;
+	int	sign;
+	int	digit;
+
+	if (!is_valid_base(base))
+		return (0);
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	digit = get_index(*str, base);
+	res = 0;
+	while (digit != -1)
+	{
+		res = res * ft_strlen(base) + digit;
+		str++;
+		digit = get_index(*str, base);
+	}
+	return (sign * res);
+}
