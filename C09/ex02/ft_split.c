5cb83a6f80b8e860303e5456e6871c5340bbe8a2
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-09-16 14:26:22 +0300

    add C09:ex02

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -6,10 +6,87 @@
 /*   By: udasgin@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 01:55:14 by udasgin           #+#    #+#             */
-/*   Updated: 2026/09/04 01:56:18 by udasgin          ###   ########.fr       */
+/*   Updated: 2026/09/16 14:26:01 by udasgin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int	is_sep(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (charset[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_count_words(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] && is_sep(str[i], charset))
+			i++;
+		if (str[i])
+			count++;
+		while (str[i] && !is_sep(str[i], charset))
+			i++;
+	}
+	return (count);
+}
+
+char	*get_word(char *str, char *charset)
+{
+	int		len;
+	int		i;
+	char	*word;
+
+	len = 0;
+	while (str[len] && !is_sep(str[len], charset))
+		len++;
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
 char	**ft_split(char *str, char *charset)
 {
+	char	**res;
+	int		i;
+	int		j;
+
+	res = malloc(sizeof(char *) * (ft_count_words(str, charset) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] && is_sep(str[i], charset))
+			i++;
+		if (str[i])
+			res[j++] = get_word(&str[i], charset);
+		while (str[i] && !is_sep(str[i], charset))
+			i++;
+	}
+	res[j] = NULL;
+	return (res);
 }
